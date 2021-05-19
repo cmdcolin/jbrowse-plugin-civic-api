45aca2549ad94742a1131f9e3b2b6e82a4562519
Colin <colin.diesh@gmail.com>
2021-05-19 18:51:02 -0400

    Add civic track automatically to hg19 assembly

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import AdapterType from "@jbrowse/core/pluggableElementTypes/AdapterType";
 import Plugin from "@jbrowse/core/Plugin";
 import { AdapterClass, configSchema } from "./CIVICAdapter";
+import { autorun } from "mobx";
 import { version } from "../package.json";
+import PluginManager from "@jbrowse/core/PluginManager";
 
 export default class CIVICPlugin extends Plugin {
   name = "CIVICPlugin";
@@ -15,5 +17,22 @@ export default class CIVICPlugin extends Plugin {
           AdapterClass,
         }),
     );
+  }
+
+  configure(pm: PluginManager) {
+    autorun(() => {
+      if (pm.rootModel?.session)
+        //@ts-ignore remove me once this is in abstract session model
+        pm.rootModel.session.addTrackConf({
+          type: "FeatureTrack",
+          trackId: "civic_hg19",
+          name: "CIVIC cancer variants hg19",
+          category: ["Annotation"],
+          assemblyNames: ["hg19"],
+          adapter: {
+            type: "CIVICAdapter",
+          },
+        });
+    });
   }
 }
